77350100fcad1b752e2a93405561fae79de4ba95
George Liu <georgeliu96@gmail.com>
2019-05-24 19:19:04 -0400

    Add better solution for 26

--- leetCode/26 Remove Duplicates from Sorted Array.js
+++ leetCode/26 Remove Duplicates from Sorted Array.js
@@ -1,6 +1,21 @@
 var removeDuplicates = function(nums) {
-    for (let i = 0; i < nums.length - 1; i++) {
-        while(nums[i] === nums[i + 1]) nums.splice(i,1);
+    // splice is slow
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     while(nums[i] === nums[i + 1]) nums.splice(i,1);
+    // }
+    // return nums.length;
+    
+    //this is much faster
+    let i = 0;
+    let j = 1;
+    while(j < nums.length) {
+        if(nums[i] === nums[j]) {
+            j++;
+        }else {
+            i++;
+            nums[i] = nums[j];
+            j++;
+        }
     }
-    return nums.length;
+    return i + 1;
 };
